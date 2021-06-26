27348ed16eff80dfddcabdf5602ee23d8baec8b9
Gatuk Chattanon <gatukgl@gmail.com>
2021-06-26 12:14:09 +0700

    Add test for making sure that get books has called

--- my-book-store/src/Books/index.test.js
+++ my-book-store/src/Books/index.test.js
@@ -1,13 +1,26 @@
-import { render, screen, act, waitFor } from '@testing-library/react'
-import MockAdapter from 'axios-mock-adapter'
-import { BrowserRouter } from 'react-router-dom'
+import { render, screen, act } from '@testing-library/react'
 import { MemoryRouter } from 'react-router'
 
 import { Books } from '.'
 import { getBooks } from '../utils/apis'
+import * as apis from '../utils/apis'
 
 jest.mock('../utils/apis', () => ({ getBooks: jest.fn() }))
 describe('Books', () => {
+  test('should call getBooks', async () => {
+    const spyGetBooks = jest.spyOn(apis, 'getBooks')
+    await getBooks.mockImplementation(() => Promise.resolve([]))
+
+    await act(async () => {
+      await render(
+        <MemoryRouter>
+          <Books />
+        </MemoryRouter>
+      )
+    })
+
+    expect(await spyGetBooks).toHaveBeenCalled()
+  })
   test('should render 0 book card when api return 0 book', async () => {
     await getBooks.mockImplementation(() => Promise.resolve([]))
     await act(async () => {
